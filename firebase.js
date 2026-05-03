a4c3dbb3395b68e04a31437ef791920e51a4ce10
THIỆP MỜI ONLINE <quychau9x@gmail.com>
2026-05-03 10:35:12 +0700

    Update Firebase configuration with additional fields
    Added storage bucket and messaging sender ID to Firebase config.

--- firebase.js
+++ firebase.js
@@ -3,11 +3,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// DÁN CONFIG BẠN COPY TỪ FIREBASE
 const firebaseConfig = {
   apiKey: "AIzaSyBpoi3xuIIiOQXMohVLOITlf0DX9JKbs-4",
   authDomain: "grouppublic-583ac.firebaseapp.com",
-  projectId: "grouppublic-583ac"
+  projectId: "grouppublic-583ac",
+  storageBucket: "grouppublic-583ac.firebasestorage.app",
+  messagingSenderId: "70167414260",
+  appId: "1:70167414260:web:8d3111a010959a38a965a1"
 };
 
 const app = initializeApp(firebaseConfig);
