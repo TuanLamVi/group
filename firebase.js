5eae4f2b748f6206cc59e2721da801b0036ea4b6
THIỆP MỜI ONLINE <quychau9x@gmail.com>
2026-05-03 10:16:53 +0700

    Update Firebase configuration details
    Updated Firebase configuration with new API key, auth domain, and project ID.

--- firebase.js
+++ firebase.js
@@ -5,9 +5,9 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 // DÁN CONFIG BẠN COPY TỪ FIREBASE
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx"
+  apiKey: "AIzaSyBpoi3xuIIiOQXMohVLOITlf0DX9JKbs-4",
+  authDomain: "grouppublic-583ac.firebaseapp.com",
+  projectId: "grouppublic-583ac"
 };
 
 const app = initializeApp(firebaseConfig);
